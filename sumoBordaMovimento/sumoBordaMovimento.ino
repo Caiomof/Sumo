799272d835963d8fdb2e8aebe98e7903d9d89bd3
Ruben <rubenfelipe15@ymail.com>
2018-05-18 22:03:10 -0300

    Atualização

--- sumoBordaMovimento/sumoBordaMovimento.ino
+++ sumoBordaMovimento/sumoBordaMovimento.ino
@@ -1,21 +1,62 @@
-#define MOTOR_E1 5
-#define MOTOR_E2 6
-#define MOTOR_D1 9
-#define MOTOR_D2 10
+#include <QTRSensors.h>
+
+#define MOTOR_E1 6 //IN1 ptH
+#define MOTOR_E2 5 //IN2 ptH
+#define MOTOR_D1 11 //IN3 ptH
+#define MOTOR_D2 10 //IN4 ptH
 
 //======DEFINIÇÃO TEMPO DA LUTA=================
 unsigned long tempoInicio = 0;
 #define TEMPO_FIM 180000 //3 Minutos
 //==============================================
 
-#define QTD_SENS_BORDA_F 6
-#define QTD_SENS_BORDA_T 2
-int *valorSensorBordaF;
-int *valorSensorBordaT;
+#define QTD_SENS_BORDA_F 8 //QTD de sensores da frente
 
-int velocidades [12] = { -255, -230, -110, -50, 50, 110, 230, 255};
+#define QTD_SENS_BORDA_T 2 //QTD de sensores de trás
+#define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
+#define EMITTER_PIN   2     // emitter is controlled by digital pin 2 (serve só para o array e, pelo Proteus ,não está conectado na placa)
+
+
+
+//int *valorSensorBordaF;
+unsigned int ValoresQtrrc8[QTD_SENS_BORDA_F];
+//int *valorSensorBordaT;
+unsigned int ValoresQtrrc2[QTD_SENS_BORDA_T];
+
+int velocidades [12] = { -255, -230, -200, 200, 230, 255};
 //===============================================================================
-void detectarBorda (int * sensoresFrente, int * sensoresTras, int * retorno)
+QTRSensorsRC qtrrc2((unsigned char[]) {A4, A5}, QTD_SENS_BORDA_T, TIMEOUT, EMITTER_PIN);
+QTRSensorsRC qtrrc8((unsigned char[]) {13, 12, 9, 8, 7, 4, 3, 2}, QTD_SENS_BORDA_F, TIMEOUT, EMITTER_PIN); 
+
+
+void lerSensorBorda(unsigned int * valSensoresBorda, int tamanhoArray)
+{
+  if (tamanhoArray == 2)
+    qtrrc2.read (valSensoresBorda);
+  else
+    qtrrc8.read (valSensoresBorda);
+    
+  for (int i = 0; i < tamanhoArray; i++)
+  {
+    valSensoresBorda[i] = ((valSensoresBorda[i] <= 1000) ? 1 : 0); //<=COR_BORDA definir #define. Vendo nada o retorno é 2500
+  }
+
+  
+  //==============================================Vai pra debug
+  for (unsigned char i = 0; i < tamanhoArray; i++)
+  {
+    Serial.print(valSensoresBorda[i]);   //Valor máximo do teste: 2500 2500. Diminue com branco
+    Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor
+  }
+  Serial.println();
+ //==============================================Vai pra debug 
+  
+}
+
+
+
+//void detectarBorda (int * sensoresFrente, int * sensoresTras, int * retorno)
+void detectarBorda (unsigned int * sensoresFrente, unsigned int * sensoresTras, int * retorno)
 {
   //'retorno' é um array de 2 posições que será preenchido com um codigo para a função de reação.
   //Os sensores da frente são positivos os 2 de trás são negativos
@@ -32,31 +73,37 @@ void detectarBorda (int * sensoresFrente, int * sensoresTras, int * retorno)
   }
   return;
 }
-boolean detectaOpon(){return false;}
+boolean detectaOpon() {
+  return false;
+}
 
 void procurar ()
 {
+  Serial.println("Procurando");
   unsigned long tempo = 0;
   int valMaxRand = (sizeof(velocidades) / sizeof(velocidades[0]));
   int *codReacao = (int *) malloc (2 * sizeof (int));
-  
+
   int randPosicao = 0;
   int velociRandE = 0;
   int velociRandD = 0;
-  
+
   do
   {
     randPosicao = random (0, valMaxRand);     //entre min=0 e max-1
-    velociRandE = velocidades[randPosicao1];
+    velociRandE = velocidades[randPosicao];
     randPosicao = random (0, valMaxRand);    //entre min=0 e max-1
-    velociRandD = velocidades[randPosicao2]; 
+    velociRandD = velocidades[randPosicao];
 
     tempo = millis();
     //inicia a movimentação com as verificações
     do
     {
       movimentacao (velociRandE, velociRandD);
-      detectarBorda(valorSensorBordaF, valorSensorBordaT, codReacao);
+      lerSensorBorda(ValoresQtrrc8,8); //preenche o array de valores
+      lerSensorBorda(ValoresQtrrc2,2); //preenche o array de valores
+      detectarBorda(ValoresQtrrc8, ValoresQtrrc2, codReacao);
+      
       if (codReacao[0] != 0 && codReacao[1] != 0)
       {
         reacaoBorda (codReacao);
@@ -67,15 +114,20 @@ void procurar ()
     tempo = 0;
 
 
-  } while ((millis() - tempoInicio) >= TEMPO_FIM);
+  } while ((millis() - tempo) <= TEMPO_FIM);
+
   free(codReacao);
+  pinMode(13, OUTPUT);
+  digitalWrite (13, HIGH);
+  delay(2000);
+  digitalWrite (13, LOW);
+  pinMode(13, INPUT);
 }
 
 void movimentacao(int potenciaE, int potenciaD) {
   motorEsq(potenciaE);
   motorDir(potenciaD);
 }
-
 
 
 void reacaoBorda (int * codigoReacao)
@@ -132,7 +184,7 @@ void reacaoBorda (int * codigoReacao)
         {
           movimentacao (255, 0);
         } while ((millis() - tempo) <= 1500);
-                 movimentacao (240, 110);
+        movimentacao (240, 110);
       } while (((millis() - tempo) <= 1000) || (codigoReacao[0] > 0 && codigoReacao[1] == 0));
       tempo = 0;
       parar();
@@ -146,7 +198,7 @@ void reacaoBorda (int * codigoReacao)
         {
           movimentacao (0, 255);
         } while ((millis() - tempo) <= 1500);
-                 movimentacao (110, 240);
+        movimentacao (110, 240);
       } while (((millis() - tempo) <= 1000) || (codigoReacao[0] == 0 && codigoReacao[1] > 0));
       tempo = 0;
       parar();
@@ -160,11 +212,11 @@ void reacaoBorda (int * codigoReacao)
         movimentacao (255, 255);
       } while ((millis() - tempo) <= 1500);
 
-               tempo = 0;
-               parar();
+      tempo = 0;
+      parar();
 
     }
-} while (codigoReacao[0] != 0 && codigoReacao[1] != 0);
+  } while (codigoReacao[0] != 0 && codigoReacao[1] != 0);
 }
 
 void parar ()
@@ -200,11 +252,23 @@ void motorEsq(int potencia)
   }
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
 
+  delay(500);
+  //(13, OUTPUT); em uso pelo arraySensor
+ // digitalWrite(13, HIGH);    // turn on Arduino's LED to indicate we are in calibration mode
+  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
+  {
+    qtrrc2.calibrate();       // reads all sensors 10 times at 2500 us per read (i.e. ~25 ms per call)
+    qtrrc8.calibrate();
+  }
+  //digitalWrite(13, LOW);     // turn off Arduino's LED to indicate we are through with calibration
+  Serial.begin(9600); // set the data rate in bits per second for serial data transmission
+  
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+  procurar ();  
+ 
 }
